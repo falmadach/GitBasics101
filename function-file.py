a5a2f45850963a790a7c9cead5d1c661465ff34c
falmadach <falmada@mgmresorts.com>
2024-06-03 11:09:50 -0700

    Data Types basics

---
+++ function-file.py
@@ -0,0 +1,11 @@
+print ("Working with Data Types in Python/n")
+
+x = 1
+print(type(x)) # outputs: <class 'int'>
+
+x = 1.0
+print(type(x)) # outputs: <class 'float'>
+
+
+x = True
+print(type(x)) # outputs: <class 'bool'>
